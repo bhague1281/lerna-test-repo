c18bdd1d2217e147f4d45fb413b6c4d6aec9d5f3
Brian Hague <brian.hague@sap.com>
2019-03-15 15:11:28 -0700

    Add release script

--- packages/lerna-constants/src/lerna-constants.js
+++ packages/lerna-constants/src/lerna-constants.js
@@ -1,3 +1,4 @@
 export const TEST1 = 'test1';
 export const TEST2 = 'test2';
 export const TEST3 = 'test3';
+export const TEST4 = 'test4';
--- packages/lerna-consumer/src/lerna-consumer.js
+++ packages/lerna-consumer/src/lerna-consumer.js
@@ -3,7 +3,7 @@ import {TEST1, TEST2, TEST3} from '@bhague1281/lerna-constants';
 export const lernaConsumer = () => {
   return {
     meta: {
-      info: 'This is a lerna consumer consuming constants'
+      info: 'This is a lerna consumer consuming constants (test)'
     },
     TEST1,
     TEST2,
